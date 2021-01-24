c2f5c44864ee33de76be2dbc1be656d944b8d547
Andrew <Designinglife11@gmail.com>
2021-01-23 20:29:36 -0500

    commit tree 75bc029d4d4c8a36ff199fac13d33dcc30279f73

--- prime.rb
+++ prime.rb
@@ -1,1 +1,9 @@
-# Add  code here!
+def prime?(input)
+    return false if input < 2
+    (2..input - 1).each do |x|
+        if (input % x) == 0
+            return false
+        end
+    end
+    true
+end
